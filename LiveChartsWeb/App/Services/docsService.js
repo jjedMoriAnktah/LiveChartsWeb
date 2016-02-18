fea9e58b3a862ff31fe643288dbcaf121a663069
beto rodriguez <rodriguezo.alberto@gmail.com>
2016-02-17 19:32:32 -0600

    Docs template finished

--- LiveChartsWeb/App/Services/docsService.js
+++ LiveChartsWeb/App/Services/docsService.js
@@ -15,9 +15,9 @@ app.factory('docsService', [function () {
 
     var componentsProps = {
         chart: [
-            { name: 'MockedArea', type: types.rect, text: 'Gets or sets an area for unit testing, since normally for unit testing you don´t have an UI and LiveCharts refuses to draw a chart if there is no a valid UI area, then you need to set this property to run unit test, width and hegiht must be grather than 15px.', tags: 'area mock unit test', behavesAs: ['static'] },
-            { name: 'Colors', type: listOf(types.color), text: 'Gets or sets the default series colors.', tags: 'color appearance style', behavesAs: ['static'] },
-            { name: 'RandomizeStartingColor', type: types.bool, text: 'Gets or sets if a each new instance of a chart should initialize with a random color index', tags: 'color appearance style', behavesAs: ['static'] },
+            { name: 'MockedArea', type: types.rect, text: 'Gets or sets an area for unit testing, since normally for unit testing you don´t have an UI and LiveCharts refuses to draw a chart if there is no a valid UI area, then you need to set this property to run unit test, width and hegiht must be grather than 15px.', tags: 'area mock unit test', behavesAs: ['Static'], defualt: 'null', example: 'Chart.MockedArea = new Rect { X = 0, Y = 0, Width = 800, Height = 600 };' },
+            { name: 'Colors', type: listOf(types.color), text: 'Gets or sets the default series colors.', tags: 'color appearance style', behavesAs: ['Static'], defualt: 'see example', example: 'Chart.Colors = new List<Color>\n{\n\tColor.FromRgb(33, 149, 242),\n\tColor.FromRgb(243, 67, 54),\n\tColor.FromRgb(254, 192, 7),\n\tColor.FromRgb(96, 125, 138),\n\tColor.FromRgb(155, 39, 175),\n\tColor.FromRgb(0, 149, 135),\n\tColor.FromRgb(76, 174, 80),\n\tColor.FromRgb(121, 85, 72),\n\tColor.FromRgb(157, 157, 157),\n\tColor.FromRgb(232, 30, 99),\n\tColor.FromRgb(63, 81, 180),\n\tColor.FromRgb(0, 187, 211),\n\tColor.FromRgb(254, 234, 59),\n\tColor.FromRgb(254, 87, 34)\n};' },
+            { name: 'RandomizeStartingColor', type: types.bool, text: 'Gets or sets if a each new instance of a chart should initialize with a random color index', tags: 'color appearance style', behavesAs: ['Static'], defualt: 'null', example: 'Chart.RandomizeStartingColor = true;' },
             { name: 'AxisY', type: {}, text: '', tags: '', behavesAs: [] },
             { name: '', type: {}, text: '', tags: '', behavesAs: [] },
             { name: '', type: {}, text: '', tags: '', behavesAs: [] },
@@ -71,14 +71,16 @@ app.factory('docsService', [function () {
             name: 'Line Chart',
             text: 'Most common chart, they are really when you need to identify data trends or variation, between one or multiple series.',
             route: 'linechart',
-            icon: 'fa-pie-chart',
+            icon: 'fa-line-chart',
+            source: 'https://github.com/beto-rodriguez/Live-Charts/blob/master/LiveChartsCore/Charts/LineChart.cs',
             getProps: function () { return buildProperties(['chart']); }
         },
         barchart: {
             name: 'Bar Chart',
             text: 'You normally use a bar chart when you need to highlight the variation between each data point, they normally require a small number of points.',
             route: 'barchart',
-            icon: 'fa-pie-chart',
+            icon: 'fa-bar-chart',
+            source: 'https://github.com/beto-rodriguez/Live-Charts/blob/master/LiveChartsCore/Charts/BarChart.cs',
             getProps: function () { return buildProperties(['chart']); }
         }
     };
---
+++ LiveChartsWeb/App/dirrectives.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+app.directive('prettyprint', function () {
+    return {
+        restrict: 'C',
+        scope: {
+            code: '='
+        },
+        link: function postLink(scope, element, attrs) {
+            element.html(prettyPrintOne(scope.code));
+        }
+    };
+});
