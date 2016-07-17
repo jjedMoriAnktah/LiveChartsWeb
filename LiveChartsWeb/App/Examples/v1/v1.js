a036180155183156526e49234fe0214cb0055839
beto rodriguez <rodriguezo.alberto@gmail.com>
2016-07-16 20:05:22 -0500

    linq

--- LiveChartsWeb/App/Examples/v1/v1.js
+++ LiveChartsWeb/App/Examples/v1/v1.js
@@ -45,8 +45,11 @@ app.factory('examplesV1Service', [
                 new article('Negative Stacked', 'basic chart bar stacked row'),
                 new article('Percentage Stacked', 'basic chart bar stacked row'),
                 new article('Dynamic Visibility', 'bar column', 'dynvis.gif'),
-                new article('Heat Series', 'heat calendar date'),
                 new article('Customizing Tooltips', 'column bar, tooltip legend custom'),
+                new article('Using LINQ', 'column bar linq', 'using linqq.gif'),
+
+                //Heat
+                new article('Heat Series', 'heat calendar date'),
 
                 //Pies
                 new article('Pie Chart', 'pie chart doughnut stacked'),
